6d6ba34a3427edca856c76fa1407bf5367715491
jooo0922 <jooo0922@gmail.com>
2024-12-19 17:24:26 +0900

    feat: render 2D sprite for test

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -48,4 +48,6 @@ void Game::ProcessInput(float dt)
 
 void Game::Render()
 {
+  // 2D Sprite 렌더링 테스트
+  Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
 }
