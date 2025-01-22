a5d8e1ee073c04ff1ffebc0455090533aeed480c
jooo0922 <jooo0922@gmail.com>
2025-01-22 21:51:34 +0900

    feat: include headers in posr_processor.hpp

---
+++ src/postprocess/post_processor.hpp
@@ -0,0 +1,11 @@
+#ifndef POST_PROCESSOR_HPP
+#define POST_PROCESSOR_HPP
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include "../utils/texture.hpp"
+#include "../utils/shader.hpp"
+#include "../renderer/sprite_renderer.hpp"
+
+#endif /* POST_PROCESSOR_HPP */
