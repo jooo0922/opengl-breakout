88cf27332cb8277f38164f7b258d57be6dac9599
jooo0922 <jooo0922@gmail.com>
2025-02-25 23:31:42 +0900

    feat: implement RenderText() func

--- src/renderer/text_renderer.cpp
+++ src/renderer/text_renderer.cpp
@@ -102,8 +102,74 @@ void TextRenderer::Load(std::string font, unsigned int fontSize)
   }
 };
 
-void TextRenderer::RenderText(std::string text, float x, float y, float scale, glm::vec3 color) {
+void TextRenderer::RenderText(std::string text, float x, float y, float scale, glm::vec3 color)
+{
+  // shader 객체 바인딩 및 색상값 전송
+  this->TextShader.Use();
+  this->TextShader.SetVec3("textColor", color);
 
+  // grayscale bitmap 텍스쳐(glyph 텍스쳐)를 바인딩할 0번 texture unit 활성화
+  glActiveTexture(0);
+
+  // glyph 텍스쳐를 적용할 2D Quad 정점 데이터 VAO 객체 바인딩
+  glBindVertexArray(this->VAO);
+
+  /** 주어진 문자열 컨테이너 std::string 을 순회하며 각 문자에 대응되는 glyph 를 2D Quad 에 렌더링  */
+  // std::string 컨테이너를 순회하는 '읽기 전용' 이터레이터 선언 (하단 필기 참고)
+  std::string::const_iterator c;
+  for (c = text.begin(); c != text.end(); c++)
+  {
+    // 현재 순회 중인 char 타입 문자에 대응되는 glyph metrices 를 가져옴
+    Character ch = Characters[*c];
+
+    // 현재 문자를 렌더링할 glyph 의 위치(= 2D Quad 의 좌상단 정점의 좌표값) 계산 (하단 필기 참고)
+    float xpos = x + ch.Bearing.x * scale;
+    float ypos = y + (this->Characters['H'].Bearing.y - ch.Bearing.y) * scale;
+
+    // 현재 문자를 렌더링할 glyph 의 크기(= 2D Quad 의 width, height) 계산
+    float w = ch.Size.x * scale;
+    float h = ch.Size.y * scale;
+
+    // glyph 의 위치(= 2D Quad 좌상단 정점)와 크기(= 2D Quad 의 width, height)를 가지고 2D Quad 정점 데이터 계산
+    // (이때, text-rendering 예제와 달리 orthogonal projection 행렬에 의해 위아래가 뒤집혔으므로, 정점 순서를 변경해서 2D Quad 의 앞/뒷면이 뒤집어지지 않도록 함.)
+    float vertices[6][4] = {
+        // position      // uv
+        {xpos, ypos + h, 0.0f, 1.0f},
+        {xpos + w, ypos, 1.0f, 0.0f},
+        {xpos, ypos, 0.0f, 0.0f},
+
+        {xpos, ypos + h, 0.0f, 1.0f},
+        {xpos + w, ypos + h, 1.0f, 1.0f},
+        {xpos + w, ypos, 1.0f, 0.0f}};
+
+    // 2D Quad 에 적용할 grayscale bitmap 텍스쳐 버퍼 바인딩
+    glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+
+    // 재계산된 2D Quad 정점 데이터를 VBO 객체에 덮어쓰기
+    glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // 2D Quad draw call
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    /**
+     * 현재 glyph 원점에서 Advance 만큼 떨어진 다음 glyph 원점의 x 좌표값 계산
+     *
+     * 이 때, FreeType 라이브러리의 Advance 값은 1/64 px 단위로 계산되기 때문에,
+     * 이것을 1px 단위로 변환해서 사용해야 함.
+     *
+     * 이를 위해 1/2^6(= 1/64)제곱값을 구해서 Advance 에 곱할 수도 있으나,
+     * >> 6, 즉, right bit shift 연산을 6번 수행하면 1/2 를 6제곱하는 것과 동일함.
+     *
+     * 심지어, bit shift 연산이 거듭제곱보다 더 빠르기 때문에, 성능 최적화에 유리함.
+     */
+    x += (ch.Advance >> 6) * scale;
+  }
+
+  // std::string 컨테이너에 저장된 모든 문자열 렌더링 완료 후, 텍스쳐 및 VAO 객체 바인딩 해제
+  glBindVertexArray(0);
+  glBindTexture(GL_TEXTURE_2D, 0);
 };
 
 /**
@@ -141,4 +207,46 @@ void TextRenderer::RenderText(std::string text, float x, float y, float scale, g
  *
  * 이렇게 하지 않으면 소위 Segmentation Fault 라고 하는
  * 허용되지 않은 에모리 영역을 침범하는 memory violation 에러가 런타임에 발생할 수 있음.
+ */
+
+/**
+ * std::string::const_iterator c
+ *
+ *
+ * 읽기 전용 이터레이터 c 에는 연속 메모리 블록으로 저장되는 std::string 컨테이너 내부의
+ * 각 char 타입의 문자들이 저장된 메모리 블록 주소값이 담겨 있음.
+ *
+ * 따라서, *c (= de-referencing)을 통해 해당 메모리에 저장된 실제 char 타입 문자에 접근 가능.
+ *
+ * 이때, for 문 내에서 c++ 연산자를 사용하면 현재 가리키고 있는 메모리 주소로부터
+ * 포인터가 가리키는 데이터 타입(= char)의 크기(= 1 byte)만큼 더해서 다음 메모리 블록에 저장된
+ * char 타입 문자를 가리키는 주소값을 얻을 수 있음.
+ *
+ * -> 즉, 연속 메모리 블록 형태로 저장된 std::string 컨테이너의 각 char 타입 값을
+ * 하나씩 순차적으로 접근할 수 있도록 for 문을 구성한 것.
+ */
+
+/**
+ * glyph 의 위치(= 2D Quad 의 좌상단 정점의 좌표값) 계산
+ *
+ *
+ * 참고로, 로컬 변수 x, y 에는 현재 렌더링할 glyph 좌상단 원점이 저장됨.
+ *
+ * text-rendering 예제와 달리, orthographic projection 행렬이
+ * 'top = 0, bottom = window height' 로 뒤집혀서 계산되므로,
+ * 현재 glyph 좌상단 원점에 Bearing 값을 더해 glyph 를 얼만큼 아래로 떨어트릴 지 결정함.
+ *
+ * 그러나, FreeType 라이브러리는 baseline(즉, glyph 원점이 포함된 수평선)에서
+ * 얼만큼 위쪽으로 떨어졌는지에 대한 offset 값만 Bearing 으로 제공하고 있을 뿐,
+ * glyph 의 천장에서 얼만큼 아래쪽으로 떨어졌는지에 대한 offset 은 제공하지 않음.
+ *
+ * 따라서, glyph 의 천장에서 얼만큼 떨어트릴지의 offset 은 직접 계산해야 되는데,
+ * 이때, 'H', 'T', 'X' 처럼 천장과의 간격이 0인 glyph 들의 Bearing.y 는
+ * 해당 폰트의 최대 높이로 가정한다면, 이 glyph 들의 높이값을 이용할 수 있음.
+ *
+ * 이 최대 높이값에 g, j, p, j 처럼 glyph 일부가 baseline(즉, glyph 원점이 포함된 수평선) 하단에 내려오는 글꼴들의,
+ * Bearing.y 값을 뺀다면, g, j, p, j 같은 glyph 들을 천장에서 아래쪽으로 얼만큼 떨어트려야 할 지 알 수 있음!
+ *
+ * 그래서 아래쪽으로 떨어트릴 offset 을
+ * y + (this->Characters['H'].Bearing.y - ch.Bearing.y) 와 같이 계산한 것!
  */
